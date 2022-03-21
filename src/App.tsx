7050c01073e3a60e7592a7ef452f722787475b8a
Felipe <figueiroa.fe@gmail.com>
2022-03-21 16:11:43 -0300

    add react-navigation to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { View, Text } from 'react-native';
 
 export const App = () => {
   return (
-    <View>
-      <Text>hello</Text>
-    </View>
+    <NavigationContainer>
+      <View>
+        <Text>hello</Text>
+      </View>
+    </NavigationContainer>
   );
 };
